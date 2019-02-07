44ed5eee05552420fb5b12fc9aac3c8fbda439cc
zhiminglim <i-jovial@hotmail.com>
2019-02-07 15:58:02 +0800

    Trip Request Fragment include RecyclerView

--- viaPorter-Yuheng/app/src/main/java/com/example/viaporter/fragments/TripRequestFragment.java
+++ viaPorter-Yuheng/app/src/main/java/com/example/viaporter/fragments/TripRequestFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +21,7 @@ public class TripRequestFragment extends Fragment {
     private static final String TAG = "viaPatron.TRFragment";
 
     private NavController navController;
+    private BroadcastAdapter mBroadcastAdapter;
 
     public TripRequestFragment() {
         // Empty constructor
@@ -37,9 +40,23 @@ public class TripRequestFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         setUpViews();
+        createAdapter();
     }
 
     private void setUpViews() {
         navController = Navigation.findNavController(getActivity(), R.id.my_nav_host_fragment);
+    }
+
+    private void createAdapter() {
+
+        Log.d(TAG, "createAdapter");
+
+        RecyclerView mBidderView = getActivity().findViewById(R.id.broadcast_request_table);
+        RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        mBidderView.setLayoutManager(linearLayoutManager);
+
+        String tempString = "Max";
+        mBroadcastAdapter = new BroadcastAdapter(tempString);
+        mBidderView.setAdapter(mBroadcastAdapter);
     }
 }
